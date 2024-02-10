f01e62026cb2206bc9fede13c82482c9c3f0d441
mcirin3 <mcirin3@uic.edu>
2024-02-10 15:32:36 -0600

    mentoring stuff

--- spark_hacks_2024/src/pages/Mentoring.js
+++ spark_hacks_2024/src/pages/Mentoring.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../App.css';
 
-
 function Mentoring() {
   return (
     <div className="mentoring">
@@ -15,10 +14,12 @@ function Mentoring() {
         <p className="mentoring-paragraph">
           UIC offers various mentoring programs where you can be successful during and after your time at UIC. Resources like one on one advising, clubs, workshops, and other events create opportunities for students to have guidance throughout the semester.
           Below are some links that the University offers depending on major. 
-          <a href="https://ecc.uic.edu/mentoring-program-2/" className="mentorship-link">COE mentorship program</a>
-          <a href="https://lasganas.uic.edu/initiatives/mentoring-con-ganas/" className="mentorship-link">L@S GANAS</a>
-          <a href="https://aarcc.uic.edu/programs/asian-american-mentor-program/" className="mentorship-link">AAMP</a>
+          
         </p>
+
+        <a href="https://ecc.uic.edu/mentoring-program-2/" className="mentorship-link">COE mentorship program</a>
+        <a href="https://lasganas.uic.edu/initiatives/mentoring-con-ganas/" className="mentorship-link">L@S GANAS</a>
+        <a href="https://aarcc.uic.edu/programs/asian-american-mentor-program/" className="mentorship-link">AAMP</a>
       </div>
     </div>
   );
