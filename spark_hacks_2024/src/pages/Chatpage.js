a23de6f79651900b6a320c6e20357d23fcf70f3c
mcirin3 <mcirin3@uic.edu>
2024-02-10 16:47:40 -0600

    new

--- spark_hacks_2024/src/pages/Chatpage.js
+++ spark_hacks_2024/src/pages/Chatpage.js
@@ -90,7 +90,7 @@ const Chatbot = () => {
           addMessage({ text: signupMessage, sender: 'bot', time: getCurrentTime() });
         }, 1000); // Add a delay of 1000 milliseconds (1 second) before the bot responds
       } 
-      else if (userMessage.toLowerCase().includes('what is this')) {
+      else if (userMessage.toLowerCase().includes('this')) {
         // If the user's message contains a greeting, respond after a delay
         setTimeout(() => {
           const purposeMessage = "The purpose of this chatbot is to assist with navigating the site and to set up appointments to meet with mentors/advisors. ";
