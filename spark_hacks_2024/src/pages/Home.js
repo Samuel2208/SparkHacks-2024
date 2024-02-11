3a3415d98cabcf7353814315a36e70724fab66f8
mcirin3 <mcirin3@uic.edu>
2024-02-11 16:38:06 -0600

    lol

--- spark_hacks_2024/src/pages/Home.js
+++ spark_hacks_2024/src/pages/Home.js
@@ -7,13 +7,12 @@ function Home() {
     <div className='home'>
       <h1> 
       <label  id='descStu'>About Our Mission</label>
-      <p id='description'>We want to create a strong community for students 
-            to get notified of events, mentoring, and student 
-            organizations where they can find their place in 
-            the University community. This website is created 
-            for the intent of giving voice to students through 
-            different services where they can learn and be included 
-            in many University activities across campus. </p>
+      <p id='description'>We aim to cultivate a vibrant and inclusive student community 
+      by providing a centralized platform for students to discover events, mentoring opportunities, 
+      and student organizations tailored to their interests and aspirations. Our website serves as a 
+      hub for students to voice their ideas, engage in diverse learning experiences, and find their place 
+      within the University community, empowering them to thrive and 
+      contribute to campus life. </p>
       </h1>
     </div>
   )
